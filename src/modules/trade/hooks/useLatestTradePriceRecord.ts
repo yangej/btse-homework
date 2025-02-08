71c6311f680d1abc324aa03933db00e68101d93d
ejyang <yangej0218@gmail.com>
2025-02-09 07:13:25 +0800

    feat: add same price condition to latest trade price record

--- src/modules/trade/hooks/useLatestTradePriceRecord.ts
+++ src/modules/trade/hooks/useLatestTradePriceRecord.ts
@@ -1,15 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { useWebSocketsContext } from '@/modules/common/web-sockets/hooks/useWebSocketsContext';
 import { BaseWebSocketResponse } from '@/modules/common/web-sockets/types';
 
 import { PriceRecord } from '../types';
 
+const SAME_PRICE_THRESHOLD = 100;
+
 export const useLatestTradePriceRecord = () => {
   const { baseWebSocket } = useWebSocketsContext();
 
   const [currPrice, setCurrPrice] = useState(0);
   const [prevPrice, setPrevPrice] = useState(0);
+
+  const countRef = useRef(0);
 
   const data: PriceRecord = { current: currPrice, previous: prevPrice };
 
@@ -30,8 +34,15 @@ export const useLatestTradePriceRecord = () => {
 
     setCurrPrice(newPrice);
 
-    if (currPrice !== newPrice) {
+    countRef.current += 1;
+
+    if (
+      currPrice !== newPrice ||
+      (currPrice === newPrice && countRef.current > SAME_PRICE_THRESHOLD)
+    ) {
       setPrevPrice(currPrice);
+
+      countRef.current = 0;
     }
   }, [baseWebSocket.message, baseWebSocket.ready, currPrice, prevPrice]);
 
