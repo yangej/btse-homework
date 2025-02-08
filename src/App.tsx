d39c0799ee7cb6f6d9c2b010de45c5e7eb3b171f
ejyang <yangej0218@gmail.com>
2025-02-08 14:18:32 +0800

    feat: put order book table on view

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import styled from 'styled-components';
+
+import { mockOderBookResponse } from './mocks/mock-order-book-data';
+import OrderBookTable from './modules/order-book/components/OrderBookTable';
+import { toOrderBook } from './modules/order-book/utils';
+
 function App() {
-  return 'App';
+  const mockTableData = toOrderBook(mockOderBookResponse);
+
+  return (
+    <Wrapper>
+      <OrderBookTable asks={mockTableData.asks} bids={mockTableData.bids} />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
 
 export default App;
