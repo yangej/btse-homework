2919e92455b97144272a871f1b8d13fe0e59f724
ejyang <yangej0218@gmail.com>
2025-02-08 14:11:19 +0800

    feat: add theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,27 @@
+export const colors = {
+  main: {
+    text: {
+      light: '#8698aa',
+      default: '#F0F4F8',
+    },
+    background: {
+      light: '#1E3059',
+      dark: '#131B29',
+    },
+    border: 'rgba(134, 152, 170, 0.12)',
+  },
+  bid: {
+    text: '#00b15d',
+    background: {
+      light: 'rgba(16, 186, 104, 0.12)',
+      dark: 'rgba(0, 177, 93, 0.5)',
+    },
+  },
+  ask: {
+    text: '#FF5B5A',
+    background: {
+      light: 'rgba(255, 90, 90, 0.12)',
+      dark: 'rgba(255, 91, 90, 0.5)',
+    },
+  },
+} as const;
